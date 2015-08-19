bbc85c170eda3655e98792b37944396e86f09b85
Dave Glover <dglover@microsoft.com>
2015-08-19 17:07:54 +1000

    refactored hcsr04 distance sensor

--- Rover The Robot/StartupTask.cs
+++ Rover The Robot/StartupTask.cs
@@ -1,273 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
-using Windows.Devices.Gpio;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
-namespace Robot
-{
-    public sealed class StartupTask : IBackgroundTask
-    {
+namespace Robot {
+    public sealed class StartupTask : IBackgroundTask {
         BackgroundTaskDeferral _deferral;
 
-        // specify which GPIO pins are wired to the left motor
-        private const int IN1 = 27;
-        private const int IN2 = 22;
-        private GpioPin leftMotorA;
-        private GpioPin leftMotorB;
+        GpioController gpio = GpioController.GetDefault();
 
-        //specify which GPIO pins are wired to the right motor
-        private const int IN3 = 6;
-        private const int IN4 = 5;
-        private GpioPin rightMotorA;
-        private GpioPin rightMotorB;
+        Motor leftMotor;
+        Motor rightMotor;
+        HCSR04 distance;
 
-        // specify which GPIO pins are wired to the distance sensor
-        private const int Trig_Pin = 23;
-        private const int Echo_Pin = 24;
-        private GpioPin trig;
-        private GpioPin echo;
-
-        // detect failures in the distance sensor
-        bool echoFailure = false;
-
-        // stopwatch to time the echo on the distance sensor
-        Stopwatch sw = new Stopwatch();
-
-        // duration of the echo
-        TimeSpan elapsedTime;
-
-        // distance between the rover and an obstacle
-        double distanceToObstacle;
-
-
-
-        public void Run(IBackgroundTaskInstance taskInstance)
-        {
-
-            var gpio = GpioController.GetDefault();
+        public void Run(IBackgroundTaskInstance taskInstance) {
 
             _deferral = taskInstance.GetDeferral();
 
-            InitGPIO();
+            if (gpio == null) { return; }
+
+            leftMotor = new Motor(gpio, 27, 22);
+            rightMotor = new Motor(gpio, 6, 5);
+            distance = new HCSR04(gpio, 23, 24);
 
             // as long as the GPIO pins initialized properly, get moving
-            while (leftMotorA != null)
-            {
+            while (true) {
                 // start moving forward
-                MoveForward();
+                Forward(100);
 
                 // as long as there is an obstacle in the way
-                while (ObstacleDetected())
-                {
-                    // initiate right turn
-
-                    
-                    TurnRight();
-
-                    // continue right turn for 250 milliseconds - a quarter turn
-                    Task.Delay(TimeSpan.FromMilliseconds(250)).Wait();
-
-                    // stop
+                while (distance.ObstacleDetected()) {
+                    Reverse(250);
+                    //TurnRight(100);
+                    //Reverse(150);
+                    TurnRight(250);
                     FullStop();
                 }
             }
-
-            // 
-            // TODO: Insert code to start one or more asynchronous methods 
-            //
         }
 
-        private void InitGPIO()
-        {
-            var gpio = GpioController.GetDefault();
+        private void Forward(uint milliseconds=0) {
+            leftMotor.Forward();
+            rightMotor.Forward();
 
-            // Show an error if there is no GPIO controller
-            if (gpio == null)
-            {
-                leftMotorA = null;
-                Debug.WriteLine("There is no GPIO controller on this device.");
-                return;
-            }
-
-            // initialize left motor
-            leftMotorA = gpio.OpenPin(IN1);
-            leftMotorB = gpio.OpenPin(IN2);
-            leftMotorA.Write(GpioPinValue.Low);
-            leftMotorB.Write(GpioPinValue.Low);
-            leftMotorA.SetDriveMode(GpioPinDriveMode.Output);
-            leftMotorB.SetDriveMode(GpioPinDriveMode.Output);
-
-            // initialize right motor
-            rightMotorA = gpio.OpenPin(IN3);
-            rightMotorB = gpio.OpenPin(IN4);
-            rightMotorA.Write(GpioPinValue.Low);
-            rightMotorB.Write(GpioPinValue.Low);
-            rightMotorA.SetDriveMode(GpioPinDriveMode.Output);
-            rightMotorB.SetDriveMode(GpioPinDriveMode.Output);
-
-            // initialize distance sensor
-            trig = gpio.OpenPin(Trig_Pin);
-            echo = gpio.OpenPin(Echo_Pin);
-            trig.SetDriveMode(GpioPinDriveMode.Output);
-            echo.SetDriveMode(GpioPinDriveMode.Input);
-            trig.Write(GpioPinValue.Low);
-
-            Debug.WriteLine("GPIO pins initialized correctly.");
-        }
-
-        private void MoveForward()
-        {
-            // spin the left motor in the forward direction
-            leftMotorA.Write(GpioPinValue.Low);
-            leftMotorB.Write(GpioPinValue.High);
-
-            // spin the right motor in the forward direction
-            rightMotorA.Write(GpioPinValue.Low);
-            rightMotorB.Write(GpioPinValue.High);
+            Task.Delay(TimeSpan.FromMilliseconds(milliseconds)).Wait();
 
             Debug.WriteLine("Forward");
         }
 
-        /* not used initially but will be needed as the robot gets smarter
-        private void MoveReverse()
-        {
-            // spin the left motor in the reverse direction
-            leftMotorA.Write(GpioPinValue.High);
-            leftMotorB.Write(GpioPinValue.Low);
+        private void Reverse(uint milliseconds = 0) {
+            leftMotor.Backward();
+            rightMotor.Backward();
 
-            // spin the right motor in the reverse direction
-            rightMotorA.Write(GpioPinValue.High);
-            rightMotorB.Write(GpioPinValue.Low);
+            Task.Delay(TimeSpan.FromMilliseconds(milliseconds)).Wait();
 
-            Direction.Text = "Reverse";
+            Debug.WriteLine("Reverse");
         }
-        */
 
-        /* not used initially but will be needed as the robot gets smarter
-        private void TurnLeft()
-        {
+
+        private void TurnLeft(uint milliseconds = 0) {
             // spin the left motor in the reverse direction
-            leftMotorA.Write(GpioPinValue.High);
-            leftMotorB.Write(GpioPinValue.Low);
+            leftMotor.Backward();
+            rightMotor.Forward();
 
-            // spin the right motor in the forward direction
-            rightMotorA.Write(GpioPinValue.Low);
-            rightMotorB.Write(GpioPinValue.High);
+            Task.Delay(TimeSpan.FromMilliseconds(milliseconds)).Wait();
 
-            Direction.Text = "Left";
+            Debug.WriteLine("Left");
         }
-        */
 
-        private void TurnRight()
-        {
-            // spin the left motor in the forward direction
-            leftMotorA.Write(GpioPinValue.Low);
-            leftMotorB.Write(GpioPinValue.High);
 
-            // spin the right motor in the reverse direction
-            rightMotorA.Write(GpioPinValue.High);
-            rightMotorB.Write(GpioPinValue.Low);
+        private void TurnRight(uint milliseconds = 0) {
+            leftMotor.Forward();
+            rightMotor.Backward();
+
+            Task.Delay(TimeSpan.FromMilliseconds(milliseconds)).Wait();
 
             Debug.WriteLine("Right");
         }
 
-        private void FullStop()
-        {
-            // Stop the left motor
-            leftMotorA.Write(GpioPinValue.Low);
-            leftMotorB.Write(GpioPinValue.Low);
+        private void FullStop(uint milliseconds = 0) {
+            leftMotor.Stop();
+            rightMotor.Stop();
 
-            // Stop the right motor
-            rightMotorA.Write(GpioPinValue.Low);
-            rightMotorB.Write(GpioPinValue.Low);
+            Task.Delay(TimeSpan.FromMilliseconds(milliseconds)).Wait();
 
             Debug.WriteLine("Neutral");
-        }
-
-        private bool ObstacleDetected()
-        {
-            // use the distance sensor to get a reading on objects in front of PiBot
-            DistanceReading();
-
-            // if something is within 30 cm or 1 foot
-            // then it is an obstacle that needs to be avoided
-            if (distanceToObstacle < 30.0)
-            {
-                Debug.WriteLine("Found at " + Convert.ToString(distanceToObstacle) + " cm");
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void DistanceReading()
-        {
-            // reset the stopwatch
-            sw.Reset();
-
-            // ensure the trigger is off
-            trig.Write(GpioPinValue.Low);
-
-            // wait for the sensor to settle
-            Task.Delay(TimeSpan.FromMilliseconds(500)).Wait();
-
-            // turn on the pulse
-            trig.Write(GpioPinValue.High);
-
-            // let the pulse run for 10 microseconds
-            Task.Delay(TimeSpan.FromMilliseconds(.01)).Wait();
-
-            // turn off the pulse
-            trig.Write(GpioPinValue.Low);
-
-            //start the stopwatch
-            sw.Start();
-
-            // wait until the echo starts
-            while (echo.Read() == GpioPinValue.Low)
-            {
-                if (sw.ElapsedMilliseconds > 1000)
-                {
-                    // if you have waited for more than a second, then there was a failure in the echo
-                    echoFailure = true;
-                    break;
-                }
-            }
-
-            if (echoFailure)
-            {
-                // reset the echoFailure in preparation for the next reading
-                echoFailure = false;
-            }
-            else
-            {
-                // echo is working properly
-                // so restart the stop watch at zero
-                sw.Restart();
-            }
-
-            // stop the stopwatch when the echo stops
-            while (echo.Read() == GpioPinValue.High) ;
-            sw.Stop();
-
-            // the duration of the echo is equal to the pulse's roundtrip time
-            elapsedTime = sw.Elapsed;
-
-            // speed of sound is 34300 cm per second or 34.3 cm per millisecond
-            // since the sound waves traveled to the obstacle and back to the sensor
-            // I am dividing by 2 to represent travel time to the obstacle
-            distanceToObstacle = elapsedTime.TotalMilliseconds * 34.3 / 2.0;
-
-            Debug.WriteLine(distanceToObstacle.ToString());
         }
     }
 }
